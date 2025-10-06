fa102719c228ecd389d2a483166f81089c19a0b2
SonoNessunk <91794916+SonoNessunk@users.noreply.github.com>
2025-10-07 01:54:09 +0200

    .

--- js/register.js
+++ js/register.js
@@ -1,5 +1,5 @@
 import { auth } from "./firebase-config.js";
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { createUserWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 
 const submit = document.getElementById('submit');
 submit.addEventListener("click", function (event) {
@@ -7,32 +7,18 @@ submit.addEventListener("click", function (event) {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
+        .then(async (userCredential) => {
             // Signed up 
             const user = userCredential.user;
+            await sendEmailVerification(user);
             alert("Success")
             window.location.href = "loged.html"
             // ...
         })
         .catch((error) => {
-            const errorCode = error.code;
+            const errorCode = error.code; Sono.Nessunk.PC25
             const errorMessage = error.message;
             alert(errorMessage)
             // ..
         });
 })
-
-const googleBtn = document.getElementById('google');
-const provider = new GoogleAuthProvider();
-
-googleBtn.addEventListener("click", () => {
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            const user = result.user;
-            alert(`Benvenuto ${user.displayName}`);
-            window.location.href = "loged.html";
-        })
-        .catch((error) => {
-            alert(error.message);
-        });
-});
